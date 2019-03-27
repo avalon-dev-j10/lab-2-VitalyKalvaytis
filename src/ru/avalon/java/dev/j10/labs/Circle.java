9e5b81b2cfaa49bc593eaa2619e26f6f8233bbf5
VitalyKalvaytis <35275702+VitalyKalvaytis@users.noreply.github.com>
2019-03-27 22:36:36 +0300

    Add files via upload

---
+++ src/ru/avalon/java/dev/j10/labs/Circle.java
@@ -0,0 +1,62 @@
+package ru.avalon.java.dev.j10.labs.shapes;
+
+/**
+ * Представление об окружности.
+ * <p>
+ * Окру́жность — замкнутая плоская кривая, которая состоит из
+ * всех точек на плоскости, равноудалённых от заданной точки.
+ *
+ * @see <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%80%D1%83%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C">Окружность</a>
+ */
+public class Circle implements Ellipse{
+    int radius;
+    int xCoordinate;
+    int yCoordinate;
+    
+    public Circle(int radius, int xCoordinate, int yCoordinate){
+        this.radius = radius;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    @Override
+    public String getName() {
+        return "Circle";
+    }
+
+    @Override
+    public float getArea() {
+        
+        float rad2 = (float) Math.pow(radius, 2);
+        
+        return (float)(rad2*3.14);
+    }
+
+    @Override
+    public int getRotation() {
+        return 0;
+    }
+
+    /*
+     * TODO: Реализовать класс 'Circle'
+     * 1. Используйте наследование.
+     * 2. Реализуйте все абстрактные методы.
+     */
+
+    @Override
+    public float getLength() {
+        float diam = radius + radius;
+        
+        return (float)(diam*3.14);
+    }
+
+    @Override
+    public float getX() {
+        return xCoordinate; 
+    }
+
+    @Override
+    public float getY() {
+        return yCoordinate;
+    }
+}
--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,11 +1,59 @@
 package ru.avalon.java.dev.j10.labs;
 
+import ru.avalon.java.dev.j10.labs.shapes.Circle;
+import ru.avalon.java.dev.j10.labs.shapes.Rectangle;
 import ru.avalon.java.dev.j10.labs.shapes.Shape;
+import ru.avalon.java.dev.j10.labs.shapes.Triangle;
 
 public class Main {
 
     public static void main(String[] args) {
-        Shape[] shapes = null;
+        Shape[] shapes = new Shape[10];
+        
+        for (int i = 0; i < 10; i++){
+            int b = (int) Math.random()*300;
+            int a = (int) (Math.random() * b);
+            int c = (int) (Math.random() * 5);
+            
+            switch(a){
+                case 0:
+                    int x = 2+c;
+                    int y = 5+c;
+                    int z = 4+c;
+                    int w = 3+c;
+                    
+                    shapes[i] = new Triangle("Triangle", x, y, z, w);
+                case 1:
+                    x = 60 + c;
+                    y = 6 + c;
+                    z = 10 + c;
+                    
+                    shapes[i] = new Rectangle(x, y, z);
+                case 2:
+                    x = 15 + c;
+                    y = 12 + c;
+                    z = 17 + c;
+                    
+                    shapes[i] = new Circle(x, y, z);    
+            }
+        }
+        int[] myArrayInt = new int[10];
+        for (int i = 0; i < 10; i++){
+            myArrayInt[i] = (int)shapes[i].getArea();
+        }
+        
+        int minArea = 0;
+        for (int i = 0; i < 10; i++){
+            if(minArea < myArrayInt[i]){
+                minArea = myArrayInt[i];
+            }
+        }
+        System.out.println(minArea);
+    }
+            
+
+}
+        
 
         /*
          * TODO: Выполнить действия над массивом 'shapes'
@@ -19,5 +67,4 @@ public class Main {
          *    площадью. Для поиска фигуры необходимо создать
          *    статический метод в текущем классе (Main).
          */
-    }
-}
+
